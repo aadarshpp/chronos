d5c1ffac96a3156c856f579586e286d16505a128
aadarshpp <aadarshaadarshpp@gmail.com>
2026-06-16 13:26:27 +0000

    Changed host url to localhost (127.0.0.1)

--- scripts/main.py
+++ scripts/main.py
@@ -110,4 +110,4 @@ async def get_api_data(start: int = None, end: int = None):
     return get_data(start, end)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
