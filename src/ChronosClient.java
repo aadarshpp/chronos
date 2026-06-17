4486319fc1075544da7c41b6b913707d42d8b73f
aadarshpp <aadarshaadarshpp@gmail.com>
2026-06-17 18:34:52 +0000

    Cleaned up Client java

--- src/ChronosClient.java
+++ src/ChronosClient.java
@@ -4,7 +4,6 @@ public class ChronosClient {
         System.loadLibrary("chronoscore");
     }
 
-    // The exact same native method from Phase 1
     public native void insertRaw(long timestamp, double price);
     
     // This class is just a wrapper.
