6577b4377d837557908b456355f1783aeab453a8
aadarshpp <aadarshaadarshpp@gmail.com>
2026-06-19 14:11:19 +0000

    Added the native query method to ChronosClient.java

--- engine/ChronosClient.java
+++ engine/ChronosClient.java
@@ -1,13 +1,11 @@
 public class ChronosClient {
-    
-    static {
-        System.loadLibrary("chronoscore");
-    }
+    static { System.loadLibrary("chronoscore"); }
 
     public native long initEngine();
     public native void insertCompressed(long ptr, long timestamp, int fixedPrice);
     public native void closeEngine(long ptr);
-    
-    // Temporary helper for Sub-Phase 4A testing
     public native String getIndexStats(long ptr);
+    
+    // The Query Method
+    public native String queryData(long startTs, long endTs);
 }
